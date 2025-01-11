259f945810892779932175db8cfe535a0116cbd2
Evgeniy Prokhorov <eprohorov@dev.vtb.ru>
2025-01-11 16:39:49 +0300

    init commit

---
+++ src/main/java/ru/prokhorov/povod/PovodApplication.java
@@ -0,0 +1,13 @@
+package ru.prokhorov.povod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PovodApplication.class, args);
+    }
+
+}
